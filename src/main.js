5f08a9d58286bece7049d7720a77959258de5d7d
youngmin jo <soser134@naver.com>
2023-09-15 18:58:08 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 const header = document.querySelector('.header');
 // header의 높이를 정의
 //getBoundingClientRect 함수를 이용해서 높이는 측정
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
     //스크롤되는 Y 좌표가 headerHeight 보다 크다면 다른 스타일링
     if(window.scrollY > headerHeight){
@@ -11,3 +11,12 @@ document.addEventListener('scroll', () => {
         header.classList.remove('header--dark');
     }
 });
+
+// Home 섹션을 아래로 스크롤 시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+
+document.addEventListener('scroll', ()=> {
+    home.style.opacity = 1-window.scrollY / homeHeight;
+});
+
