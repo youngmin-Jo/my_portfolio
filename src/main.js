3ebdb86ebe22e265ee44dc9525706dd2d02ee042
youngmin jo <soser134@naver.com>
2023-09-16 04:37:13 +0900

    JS-ArrowUp

--- src/main.js
+++ src/main.js
@@ -20,3 +20,14 @@ document.addEventListener('scroll', ()=> {
     home.style.opacity = 1-window.scrollY / homeHeight;
 });
 
+// Arrow up버튼을 아래로 스크롤시 투명하게 처리함
+const arrowUp = document.querySelector('.arrow-up');
+
+document.addEventListener('scroll',()=>{
+    if(window.scrollY > homeHeight / 2) {
+        arrowUp.style.opacity = 1;
+    } else {
+        arrowUp.style.opacity = 0;
+    }
+});
+
