0fcca19df81f3901786af82c8861cade69949470
youngmin jo <soser134@naver.com>
2023-09-16 17:07:02 +0900

    JS, CSS - toggle

--- src/main.js
+++ src/main.js
@@ -31,3 +31,11 @@ document.addEventListener('scroll',()=>{
     }
 });
 
+// Navbar 토글버튼 클릭 처리
+
+const navbarMenu = document.querySelector('.header__menu');
+
+const navbarToggle = document.querySelector('.header__toggle');
+navbarToggle.addEventListener('click', ()=> {
+    navbarMenu.classList.toggle('open');
+})
