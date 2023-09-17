a145c7ca8f56e9f5c24cc630ee9e923a6cf78aaf
youngmin jo <soser134@naver.com>
2023-09-17 20:34:02 +0900

    JS-Projects

--- src/projects.js
+++ src/projects.js
@@ -1,1 +1,37 @@
 'use strict';
+
+// 프로젝트 필터링 관련 로직 처리
+
+const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project');
+const projectsContainer = document.querySelector('.projects');
+
+categories.addEventListener('click', (event)=>{
+    const filter = event.target.dataset.category;  
+    if(filter == null){
+        return;
+    }
+    handleActiveSelection(event.target);
+    filterProjects(filter);
+});
+
+function handleActiveSelection(target){
+        //Active 메뉴를 재설정
+        const active = document.querySelector('.category--selected');
+        active.classList.remove('category--selected');
+        target.classList.add('category--selected');
+}
+        // 프로젝트 필터링
+function filterProjects(filter){
+        projectsContainer.classList.add('anim-out');
+        projects.forEach(projects => {
+            if(filter === 'all' || filter === projects.dataset.type){
+                projects.style.display = 'block';
+            }else{
+                projects.style.display = 'none';
+            }
+        });
+        setTimeout(()=>{
+        projectsContainer.classList.remove('anim-out');
+        }, 250);
+}
