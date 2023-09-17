f827966681aa0ec7d2d3373061f1eb945db062b2
youngmin jo <soser134@naver.com>
2023-09-17 19:47:56 +0900

    JS-Menu

--- src/main.js
+++ src/main.js
@@ -32,10 +32,14 @@ document.addEventListener('scroll',()=>{
 });
 
 // Navbar 토글버튼 클릭 처리
-
 const navbarMenu = document.querySelector('.header__menu');
 
 const navbarToggle = document.querySelector('.header__toggle');
 navbarToggle.addEventListener('click', ()=> {
     navbarMenu.classList.toggle('open');
-})
+});
+
+// Navbar 메뉴 클릭 시 메뉴를 자동으로 닫아줌
+navbarMenu.addEventListener('click', ()=> {
+    navbarMenu.classList.remove('open');
+});
