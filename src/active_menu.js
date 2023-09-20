7d6ab9dbb0aad07143adff75dfee59a8d0358dae
youngmin jo <soser134@naver.com>
2023-09-20 22:01:53 +0900

    Observer-part

--- src/active_menu.js
+++ src/active_menu.js
@@ -6,37 +6,56 @@
 // - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택
 // - 마지막 contact 섹션이 보여진다면, 가장 마지막 섹션을 선택
 
-const sectionIds = ['#home', '#about', '#skills', '#work', '#testimonial', '#contact',];
-const sections = sectionIds.map(id => document.querySelector(id));
-const navItems = sectionIds.map(id => document.querySelector(`[href="${id}"]`));
-
-const visibleSections = sectionIds.map(()=> false);
-
-const options = {
+const sectionIds = [
+    '#home',
+    '#about',
+    '#skills',
+    '#work',
+    '#testimonial',
+    '#contact',
+  ];
+  const sections = sectionIds.map((id) => document.querySelector(id));
+  const navItems = sectionIds.map((id) =>
+    document.querySelector(`[href="${id}"]`)
+  );
+  
+  const visibleSections = sectionIds.map(() => false);
+  let activeNavItem = navItems[0];
+  
+  const options = {
     rootMargin: '-20% 0px 0px 0px',
-    threshold: [0, 0.98],
-};
-
-const observer = new IntersectionObserver(observerCallback, options);
-
-sections.forEach((section) => observer.observe(section));
-
-function observerCallback(entries){
+    threshold: [0, 0.96],
+  };
+  const observer = new IntersectionObserver(observerCallback, options);
+  sections.forEach((section) => observer.observe(section));
+  
+  function observerCallback(entries) {
     let selectLastOne;
-    entries.forEach((entry) =>{
-        const index = sectionIds.indexOf(`#${entry.target.id}`);
-        visibleSections[index] = entry.isIntersecting;
-        selectLastOne = index === sectionIds.length -1 && entry.isIntersecting && entry.IntersectionRatio >= 0.95;
+    entries.forEach((entry) => {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      visibleSections[index] = entry.isIntersecting;
+      selectLastOne =
+        index === sectionIds.length - 1 &&
+        entry.isIntersecting &&
+        entry.intersectionRatio >= 0.93;
     });
-    console.log(visibleSections);
-    console.log('무조건 라스트 섹션!!', selectLastOne);
-    const navIndex = selectLastOne 
-    ? sectionIds.length -1 
-    : findFirstIntersection(visibleSections);
-    console.log(sectionIds[navIndex]);
-}
-
-function findFirstIntersection(intersections){
+  
+    const navIndex = selectLastOne
+      ? sectionIds.length - 1
+      : findFirstIntersecting(visibleSections);
+    selectNavItem(navIndex);
+  }
+  
+  function findFirstIntersecting(intersections) {
     const index = intersections.indexOf(true);
     return index >= 0 ? index : 0;
-}
+  }
+  
+  function selectNavItem(index) {
+    const navItem = navItems[index];
+    if (!navItem) return;
+    activeNavItem.classList.remove('active');
+    activeNavItem = navItem;
+    activeNavItem.classList.add('active');
+  }
+  
