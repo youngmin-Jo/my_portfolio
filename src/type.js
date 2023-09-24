b41ca67be9aabbbbf112faf60fa7efebd03f1b33
youngmin jo <soser134@naver.com>
2023-09-24 23:44:39 +0900

    type.js add

--- src/type.js
+++ src/type.js
@@ -1,3 +1,23 @@
 'use strict';
 
-new TypeIt('.home__title--strong').go();
+new TypeIt('.home__title--strong',{
+    loop: true, //반복
+    speed: 100, //타자 속도
+})
+.move(-20)
+.type('Amazing ')
+.pause(1000)
+.move(null, {to: 'END'})
+.delete()
+.type('Front-end Engineer')
+.pause(2000)
+.move(-9)
+.delete(9)
+.type('Back-end')
+.pause(2000)
+.delete(8)
+.type('Full-stack')
+.pause(2000)
+.move(9)
+.delete()
+.go();
